a793bbc6033ea78c1234a4aeda13727d76198471
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-14 11:02:23 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,7 @@
 import axios from "axios";
-const API = process.env.REACT_APP_API_URL || "http://localhost:10000";
 
-export function setAuthHeader(token) {
-  axios.defaults.headers.common["Authorization"] = token ? `Bearer ${token}` : "";
-}
+const API_URL = process.env.REACT_APP_API_URL;
 
-export async function post(path, data) {
-  return (await axios.post(`${API}${path}`, data)).data;
-}
-export async function get(path) {
-  return (await axios.get(`${API}${path}`)).data;
-}
+export const api = axios.create({
+  baseURL: API_URL,
+});
