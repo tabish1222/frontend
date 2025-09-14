54a0d53ad5537ab5dc6a43c295a8359927ba685b
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-14 11:06:05 +0530

    Create Dashboard.js

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div style={{ margin: "50px" }}>
+      <h2>Dashboard</h2>
+      <a href="/students">View Students</a>
+    </div>
+  );
+}
+
+export default Dashboard;
