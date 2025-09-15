b5b35609a0fe27907c06daaa0c72168a32536c8a
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-15 23:01:53 +0530

    Update students.js

--- src/services/students.js
+++ src/services/students.js
@@ -1,17 +1,10 @@
+// src/services/students.js
 import { apiRequest } from "./api";
 
-export function getStudents(token) {
-  return apiRequest("/api/students", "GET", null, token);
+export function getStudents() {
+  return apiRequest("/api/students", "GET");
 }
 
-export function addStudent(student, token) {
-  return apiRequest("/api/students", "POST", student, token);
-}
-
-export function updateStudent(id, student, token) {
-  return apiRequest(`/api/students/${id}`, "PUT", student, token);
-}
-
-export function deleteStudent(id, token) {
-  return apiRequest(`/api/students/${id}`, "DELETE", null, token);
+export function addStudent(student) {
+  return apiRequest("/api/students", "POST", student);
 }
