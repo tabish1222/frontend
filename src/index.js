6d6809b4c2ae54d1c286cc289c3e895763d461ed
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-15 22:21:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import "./index.css";   // <--- this import expects index.css to exist
 import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
