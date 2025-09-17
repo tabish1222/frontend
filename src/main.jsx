5f68be43630345a6b89ea13429ffa8c3b74f769b
tabish1222 <107319072+tabish1222@users.noreply.github.com>
2025-09-17 17:01:11 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // your App component
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
